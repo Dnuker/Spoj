0f24fc5db1eeeea7b5718cf65cf78666556262af
Robert Maciag <rix4141@gmail.com>
2022-03-20 18:25:43 +0100

    Divisibility end

--- src/Divisibility.java
+++ src/Divisibility.java
@@ -1,14 +1,32 @@
 import java.util.Scanner;  // Import the Scanner class
+
 //https://www.spoj.com/problems/SMPDIV/
 class Divisibility {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        int n = scan.nextInt();
         int x = scan.nextInt();
+        int y = scan.nextInt();
 
-        if (x == 20) {
+        while (t != 0) {
 
-            System.out.println("Primary numbers " + x);
+            for(int i=1 ;i<=n ; i++) {
+                int r = i % x;
+                int b = i % y;
+               if(r==0 && b!=0) {
+
+                   System.out.println(i);
+               }
+            }
+
+            t--;
+            if(t!=0) {
+                n = scan.nextInt();
+                x = scan.nextInt();
+                y = scan.nextInt();
+            }
+
         }
-
     }
 }
