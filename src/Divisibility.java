2678b5a133d8fae33c189b07b1d1e696138e588e
Robert Maciag <rix4141@gmail.com>
2022-03-20 19:15:50 +0100

    Divisibility refactor

--- src/Divisibility.java
+++ src/Divisibility.java
@@ -4,29 +4,20 @@ import java.util.Scanner;  // Import the Scanner class
 class Divisibility {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        int n = scan.nextInt();
-        int x = scan.nextInt();
-        int y = scan.nextInt();
+        int testCases = scan.nextInt();
+        int x, y, n;
 
-        while (t != 0) {
-
-            for(int i=1 ;i<=n ; i++) {
+        for (int z = 0; z < testCases; z++) {
+            n = scan.nextInt();
+            x = scan.nextInt();
+            y = scan.nextInt();
+            for (int i = 1; i <= n; i++) {
                 int r = i % x;
                 int b = i % y;
-               if(r==0 && b!=0) {
-
-                   System.out.println(i);
-               }
+                if (r == 0 && b != 0) {
+                    System.out.println(i);
+                }
             }
-
-            t--;
-            if(t!=0) {
-                n = scan.nextInt();
-                x = scan.nextInt();
-                y = scan.nextInt();
-            }
-
         }
     }
 }
