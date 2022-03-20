3630d9f9bdfe06f8904b04ff3b7eab89960754bf
Robert Maciag <rix4141@gmail.com>
2022-03-20 16:48:52 +0100

    initial commit

---
+++ src/Divisibility.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;  // Import the Scanner class
+//https://www.spoj.com/problems/SMPDIV/
+class Divisibility {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int x = scan.nextInt();
+
+        if (x == 20) {
+
+            System.out.println("Primary numbers " + x);
+        }
+
+    }
+}
