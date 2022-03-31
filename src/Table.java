7e2955ea2894c5062ef2379f0c7390f6b1871995
Robert Maciag <rix4141@gmail.com>
2022-03-31 16:09:14 +0200

    Table end

--- src/Table.java
+++ src/Table.java
@@ -13,16 +13,26 @@ public class Table {
             String numInputWoaSpaces = numInput.replace(" ", "");
             int arraysize = numInputWoaSpaces.length()-1 ;
             int stringLength = numInputWoaSpaces.length() ;
+
+            String[] splitStr = numInput.split("\\s+");
              //System.out.println(arraysize);
             //for (int index = 0; index < arraysize; index++) {
                 //tworzy tablice zaleznie od tego ile znakow wpiszemy
-
+            int wartosci= splitStr.length;
                 for (int x = arraysize; x >= 0; x--) {
 
-                    char[] numbers = new char[arraysize];
-                    char value = numInputWoaSpaces.charAt(x);
+                    String[] numbers = new String[arraysize];
+
+
+                    String value = splitStr[wartosci-1];
                     numbers[arraysize-1] = value;
                     System.out.println(value);
+
+                    wartosci--;
+                    if (wartosci==0)
+                    {
+                        break;
+                    }
                     // System.out.println(index);
                // }
             }
