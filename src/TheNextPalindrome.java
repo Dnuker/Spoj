efd7c8475dcb14959f3e20100a99091d3e2646ca
dnuker <rix4141@gmail.com>
2022-12-19 19:44:48 +0100

    Added testcases, deleted unnecessary code

--- src/TheNextPalindrome.java
+++ src/TheNextPalindrome.java
@@ -1,49 +1,43 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 //https://www.spoj.com/problems/PALIN/
-//chyba lepszym pomysłem bedzie wpierw przekonwertowanie liczby do array int a potem reevert
+
 public class TheNextPalindrome {
-  /*     public static int[] toIntArray(List<Integer> list) {
-            int[] ret = new int[list.size()];
-            for(int i = 0; i < ret.length; i++)
-                ret[i] = list.get(i);
-            return ret;
-        }
-*/
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int givenNumber = scan.nextInt();
-        List<String> numberInString = new ArrayList();
-        while (givenNumber!=-300) {
-            //splituje cyfre
-            String givenNumberInString = Integer.toString(givenNumber);
-            String[] givenNumberSplitted = givenNumberInString.split("");
-            int lengthOfSplittedArray = givenNumberSplitted.length - 1;
-            //to obraca liczbe
-            for (int i = lengthOfSplittedArray; i >= 0; i--) {
-                String splittednumber = givenNumberSplitted[i];
-                numberInString.add(splittednumber);
-            }
-            String foldedNumber = "";
-            //tu mam tabele z liczbą teraz tylko złożyć
-            for (int i = 0; i <= lengthOfSplittedArray; i++) {
-                foldedNumber = foldedNumber + numberInString.get(i);
-            }
+        int testcases = scan.nextInt();
+        for (int i = 0; i < testcases; i++) {
+            int givenNumber = scan.nextInt();
+            List<String> numberInString = new ArrayList();
+            while (givenNumber != -300) {
+                //splituje cyfre
+                String givenNumberInString = Integer.toString(givenNumber);
+                String[] givenNumberSplitted = givenNumberInString.split("");
+                int lengthOfSplittedArray = givenNumberSplitted.length - 1;
+                //to obraca liczbe
+                for (int j = lengthOfSplittedArray; j >= 0; j--) {
+                    String splittednumber = givenNumberSplitted[j];
+                    numberInString.add(splittednumber);
+                }
+                String foldedNumber = "";
+                //tu mam tabele z liczbą teraz tylko złożyć
+                for (int k = 0; k <= lengthOfSplittedArray; k++) {
+                    foldedNumber = foldedNumber + numberInString.get(k);
+                }
 
-            int foldedReversedNumberInInt = Integer.parseInt(foldedNumber);
-            int givenNumberInInt = Integer.parseInt(givenNumberInString);
+                int foldedReversedNumberInInt = Integer.parseInt(foldedNumber);
+                int givenNumberInInt = Integer.parseInt(givenNumberInString);
 
-            if (givenNumberInInt == foldedReversedNumberInInt) {
-                System.out.println(foldedReversedNumberInInt);
-                return;
-            } else {
-                givenNumber++;
-                numberInString.clear();
+                if (givenNumberInInt == foldedReversedNumberInInt) {
+                    System.out.println(foldedReversedNumberInInt);
+                    break;
+                } else {
+                    givenNumber++;
+                    numberInString.clear();
+                }
             }
-
         }
     }
 }
