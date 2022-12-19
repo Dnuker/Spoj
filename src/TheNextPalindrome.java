17f34c7b1d0e2c3078183ab1efc135816dbc6755
dnuker <rix4141@gmail.com>
2022-12-19 19:23:16 +0100

    Finally ended

--- src/TheNextPalindrome.java
+++ src/TheNextPalindrome.java
@@ -1,44 +1,49 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 //https://www.spoj.com/problems/PALIN/
-
+//chyba lepszym pomysłem bedzie wpierw przekonwertowanie liczby do array int a potem reevert
 public class TheNextPalindrome {
+  /*     public static int[] toIntArray(List<Integer> list) {
+            int[] ret = new int[list.size()];
+            for(int i = 0; i < ret.length; i++)
+                ret[i] = list.get(i);
+            return ret;
+        }
+*/
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String givenNumber = scan.next();
-        List<String> reversedNumberInString = new ArrayList();
-        List<Integer> reversedNumberInIntList = new ArrayList();
+        int givenNumber = scan.nextInt();
+        List<String> numberInString = new ArrayList();
+        while (givenNumber!=-300) {
+            //splituje cyfre
+            String givenNumberInString = Integer.toString(givenNumber);
+            String[] givenNumberSplitted = givenNumberInString.split("");
+            int lengthOfSplittedArray = givenNumberSplitted.length - 1;
+            //to obraca liczbe
+            for (int i = lengthOfSplittedArray; i >= 0; i--) {
+                String splittednumber = givenNumberSplitted[i];
+                numberInString.add(splittednumber);
+            }
+            String foldedNumber = "";
+            //tu mam tabele z liczbą teraz tylko złożyć
+            for (int i = 0; i <= lengthOfSplittedArray; i++) {
+                foldedNumber = foldedNumber + numberInString.get(i);
+            }
 
-        int reversedNumber = 0;
-        String[] givenNumberSplitted = givenNumber.split("");
-        int lengthOfSplittedArray= givenNumberSplitted.length-1;
-        for(int i=lengthOfSplittedArray; i>=0;i--){
-            String splittednumber=givenNumberSplitted[i];
-            reversedNumberInString.add(splittednumber);
-        }
-            //zmiana listy stringów na liste intów
-        for(String current:reversedNumberInString){
-            reversedNumberInIntList.add(Integer.parseInt(current));
-        }
+            int foldedReversedNumberInInt = Integer.parseInt(foldedNumber);
+            int givenNumberInInt = Integer.parseInt(givenNumberInString);
 
-        int[] reversedNumberInIntArray(List<Integer> reversedNumberInIntList) {
-            int[] ret = new int[reversedNumberInIntList.size()];
-            for(int i = 0; i < ret.length; i++)
-                ret[i] = reversedNumberInIntList.get(i);
-            return ret;
-        } //watafuq is that co to za bledy
-        //odnośnik https://stackoverflow.com/questions/960431/how-can-i-convert-listinteger-to-int-in-java
-        System.out.println(reversedNumberInIntArray);
-
-
-   /*         if (givenNumber == reversedNumber) {
-                System.out.println(i);
+            if (givenNumberInInt == foldedReversedNumberInInt) {
+                System.out.println(foldedReversedNumberInInt);
                 return;
             } else {
-                reversedNumber = 0;
                 givenNumber++;
+                numberInString.clear();
             }
-   */
+
+        }
     }
 }
