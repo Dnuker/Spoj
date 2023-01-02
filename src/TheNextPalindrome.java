9e9f630ab79811b7a5eb701ea9d770d7f617b9db
dnuker <rix4141@gmail.com>
2023-01-02 15:23:29 +0100

    added functions, changed names

--- src/TheNextPalindrome.java
+++ src/TheNextPalindrome.java
@@ -1,41 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 //https://www.spoj.com/problems/PALIN/
+//zmien na chary
+//2 podejscie do nazewnictwa
+//wywalic kod do funkcji do funkcji
 
 public class TheNextPalindrome {
+   public static String[] splitString(String stringToSplit) {
+                String[] givenNumberSplitted = stringToSplit.split("");
+                return givenNumberSplitted;
+
+    }
+    public static List<String> reverseNumber(int length,String numberToReverse){
+        List<String> numberInString = new ArrayList();
+        for (int j = length; j >= 0; j--) {
+            String splittednumber = splitString(numberToReverse)[j];
+            numberInString.add(splittednumber);
+        }
+        return numberInString;
+    }
+   public static String foldNumber(int lengthOfSplittedArray, List<String> numberInStringList) {
+       String foldedNumber = "";
+       for (int k = 0; k <= lengthOfSplittedArray; k++) {
+           foldedNumber = foldedNumber + numberInStringList.get(k);
+       }
+       return foldedNumber;
+   }
+
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int testcases = scan.nextInt();
-        for (int i = 0; i < testcases; i++) {
+        int testCaseCount = scan.nextInt();
+        for (int i = 0; i < testCaseCount; i++) {
             int givenNumber = scan.nextInt();
-            List<String> numberInString = new ArrayList();
-            while (givenNumber != -300) {
-                //splituje cyfre
-                String givenNumberInString = Integer.toString(givenNumber);
-                String[] givenNumberSplitted = givenNumberInString.split("");
-                int lengthOfSplittedArray = givenNumberSplitted.length - 1;
-                //to obraca liczbe
-                for (int j = lengthOfSplittedArray; j >= 0; j--) {
-                    String splittednumber = givenNumberSplitted[j];
-                    numberInString.add(splittednumber);
-                }
-                String foldedNumber = "";
-                //tu mam tabele z liczbą teraz tylko złożyć
-                for (int k = 0; k <= lengthOfSplittedArray; k++) {
-                    foldedNumber = foldedNumber + numberInString.get(k);
-                }
 
-                int foldedReversedNumberInInt = Integer.parseInt(foldedNumber);
-                int givenNumberInInt = Integer.parseInt(givenNumberInString);
-
-                if (givenNumberInInt == foldedReversedNumberInInt) {
-                    System.out.println(foldedReversedNumberInInt);
+            while (true) {
+               String givenNumberInString = Integer.toString(givenNumber);
+                int lengthOfSplittedArray = splitString(givenNumberInString).length - 1;
+                int foldedReversedNumber = Integer.parseInt(foldNumber(lengthOfSplittedArray,reverseNumber(lengthOfSplittedArray,givenNumberInString)));
+                if (givenNumber == foldedReversedNumber) {
+                    System.out.println(foldedReversedNumber);
                     break;
                 } else {
                     givenNumber++;
-                    numberInString.clear();
                 }
             }
         }
