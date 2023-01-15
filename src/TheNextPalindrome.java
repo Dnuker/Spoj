877e3be097354173e82137f4d0bb7fd7b9b0af8d
dnuker <rix4141@gmail.com>
2023-01-16 00:13:26 +0100

    fin

--- src/TheNextPalindrome.java
+++ src/TheNextPalindrome.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Collections;
-import java.util.Arrays;
+
 //https://www.spoj.com/problems/PALIN/
 
 
@@ -11,16 +11,7 @@ public class TheNextPalindrome {
                 char[] givenNumberSplittedChar=stringToSplit.toCharArray();
                 return givenNumberSplittedChar;
     }
-    public static List<Character> reverseNumber(String numberToReverse)
-    {      int length = splitString(numberToReverse).length-1;
 
-        List<Character> numberInChar = new ArrayList();
-        for (int j = length; j >= 0; j--) {
-            char splittednumber = splitString(numberToReverse)[j];
-            numberInChar.add(splittednumber);
-        }
-        return numberInChar;
-    }
    public static String foldNumber(List<Character> numberInCharList) {
        int lengthOfSplittedArray=numberInCharList.size()-1;
        String foldedNumber = "";
@@ -39,9 +30,15 @@ public class TheNextPalindrome {
 
             while (true) {
                String givenNumberInString = Integer.toString(givenNumber);
+                char[] givenNumberInCharArray= splitString(givenNumberInString);
+                List <Character> givenNumberInCharList = new ArrayList();
+                int lengthOfCharArray = givenNumberInCharArray.length;
+                for(int j=0;j<lengthOfCharArray;j++){
+                    givenNumberInCharList.add(givenNumberInCharArray[j]);
+                }
+                Collections.reverse(givenNumberInCharList);
 
-
-                int foldedReversedNumber = Integer.parseInt(foldNumber(reverseNumber(givenNumberInString)));
+                int foldedReversedNumber = Integer.parseInt(foldNumber(givenNumberInCharList));
                 if (givenNumber == foldedReversedNumber) {
                     System.out.println(foldedReversedNumber);
                     break;
