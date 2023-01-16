76ce33dcf5187a4614dcf87ebc444ff41845a285
dnuker <rix4141@gmail.com>
2023-01-16 21:51:26 +0100

    last changes

--- src/TheNextPalindrome.java
+++ src/TheNextPalindrome.java
@@ -7,21 +7,22 @@ import java.util.Collections;
 
 
 public class TheNextPalindrome {
-   public static char[] splitString(String stringToSplit) {
-                char[] givenNumberSplittedChar=stringToSplit.toCharArray();
-                return givenNumberSplittedChar;
+   public static List<Character> arrayToList(char[] givenNumberInCharArray) {
+       List <Character> givenNumberInCharList = new ArrayList();
+       int lengthOfCharArray = givenNumberInCharArray.length;
+       for(int j=0;j<lengthOfCharArray;j++){
+           givenNumberInCharList.add(givenNumberInCharArray[j]);
+       }
+       return givenNumberInCharList;
     }
 
    public static String foldNumber(List<Character> numberInCharList) {
-       int lengthOfSplittedArray=numberInCharList.size()-1;
        String foldedNumber = "";
-       for (int k = 0; k <= lengthOfSplittedArray; k++) {
+       for (int k = 0; k < numberInCharList.size(); k++) {
            foldedNumber = foldedNumber + numberInCharList.get(k);
        }
        return foldedNumber;
    }
-
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int testCaseCount = scan.nextInt();
@@ -30,12 +31,8 @@ public class TheNextPalindrome {
 
             while (true) {
                String givenNumberInString = Integer.toString(givenNumber);
-                char[] givenNumberInCharArray= splitString(givenNumberInString);
-                List <Character> givenNumberInCharList = new ArrayList();
-                int lengthOfCharArray = givenNumberInCharArray.length;
-                for(int j=0;j<lengthOfCharArray;j++){
-                    givenNumberInCharList.add(givenNumberInCharArray[j]);
-                }
+                char[] givenNumberInCharArray= givenNumberInString.toCharArray();
+                List <Character> givenNumberInCharList = arrayToList(givenNumberInCharArray);
                 Collections.reverse(givenNumberInCharList);
 
                 int foldedReversedNumber = Integer.parseInt(foldNumber(givenNumberInCharList));
