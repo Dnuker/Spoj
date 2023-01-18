0a9fda6b41ff996ba49e64ac54f138b442834189
dnuker <rix4141@gmail.com>
2023-01-18 03:47:41 +0100

    deleted unnessesary code

--- src/PeselCheck.java
+++ src/PeselCheck.java
@@ -3,48 +3,42 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class PeselCheck {
-    public static char[] dissasemblePesel(long pesel) {
-       char[] peselInCharArr = Long.toString(pesel).toCharArray();
-       return peselInCharArr;
-    }
-    public static boolean validatePesel(int[] disassembledPesel){
-        int multipliedByThree = (disassembledPesel[1]*3)+(disassembledPesel[5]*3)+(disassembledPesel[9]*3);
-        int multipliedBySeven = (disassembledPesel[2]*7)+(disassembledPesel[6]*7);
-        int multipliedByNine = (disassembledPesel[3]*9)+(disassembledPesel[7]*9);
-        int peselSum = multipliedByThree+multipliedBySeven+multipliedByNine+disassembledPesel[0]+disassembledPesel[4]+disassembledPesel[8]+disassembledPesel[10];
-        String peselSumInString  = Integer.toString(peselSum);
+
+    public static boolean validatePesel(int[] disassembledPesel) {
+        int multipliedByThree = (disassembledPesel[1] * 3) + (disassembledPesel[5] * 3) + (disassembledPesel[9] * 3);
+        int multipliedBySeven = (disassembledPesel[2] * 7) + (disassembledPesel[6] * 7);
+        int multipliedByNine = (disassembledPesel[3] * 9) + (disassembledPesel[7] * 9);
+        int peselSum = multipliedByThree + multipliedBySeven + multipliedByNine + disassembledPesel[0] + disassembledPesel[4] + disassembledPesel[8] + disassembledPesel[10];
+        String peselSumInString = Integer.toString(peselSum);
         int[] disassembledPeselSum = new int[peselSumInString.length()];
-        for (int i = 0; i < peselSumInString.length(); i++)
-        {
+        for (int i = 0; i < peselSumInString.length(); i++) {
             disassembledPeselSum[i] = peselSumInString.charAt(i) - '0';
         }
-        int thirdNumber=disassembledPeselSum[2];
-        if(thirdNumber==0){
+        int thirdNumber = disassembledPeselSum[2];
+        if (thirdNumber == 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    public static int[] convertToIntArr(String pesel){
-      int[] intArr = Stream.of(pesel.split("")).mapToInt(Integer::parseInt).toArray();
+
+    public static int[] disassemblePesel(String pesel) {
+        int[] intArr = Stream.of(pesel.split("")).mapToInt(Integer::parseInt).toArray();
         return intArr;
     }
-
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int testCaseCount = scan.nextInt();
         for (int i = 0; i < testCaseCount; i++) {
-    String pesel = scan.next();
-    int[] dissasembledPeselInIntArr = convertToIntArr(pesel);
-    if(validatePesel(dissasembledPeselInIntArr)==true){
-        System.out.println("T");
-    }
-    else{
-        System.out.println("N");
-    }
-    }
+            String pesel = scan.next();
+            int[] disassembledPesel = disassemblePesel(pesel);
+            if (validatePesel(disassembledPesel) == true) {
+                System.out.println("T");
+            } else {
+                System.out.println("N");
+            }
+        }
     }
 }
