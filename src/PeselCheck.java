f518930286914ef631af5f5bce80eb39fff70174
dnuker <rix4141@gmail.com>
2023-01-18 18:48:21 +0100

    end of task

--- src/PeselCheck.java
+++ src/PeselCheck.java
@@ -4,27 +4,13 @@ import java.util.stream.Stream;
 
 public class PeselCheck {
     public static int[] disassemblePesel(String pesel) {
-        int[] intArr = Stream.of(pesel.split("")).mapToInt(Integer::parseInt).toArray();
-        return intArr;
+        return Stream.of(pesel.split("")).mapToInt(Integer::parseInt).toArray();
     }
 
     public static boolean validatePesel(String pesel) {
         int[] disassembledPesel = disassemblePesel(pesel);
-        int multipliedByThree = (disassembledPesel[1] * 3) + (disassembledPesel[5] * 3) + (disassembledPesel[9] * 3);
-        int multipliedBySeven = (disassembledPesel[2] * 7) + (disassembledPesel[6] * 7);
-        int multipliedByNine = (disassembledPesel[3] * 9) + (disassembledPesel[7] * 9);
-        int peselSum = multipliedByThree + multipliedBySeven + multipliedByNine + disassembledPesel[0] + disassembledPesel[4] + disassembledPesel[8] + disassembledPesel[10];
-        String peselSumInString = Integer.toString(peselSum);
-        int[] disassembledPeselSum = new int[peselSumInString.length()];
-        for (int i = 0; i < peselSumInString.length(); i++) {
-            disassembledPeselSum[i] = peselSumInString.charAt(i) - '0';
-        }
-        int thirdNumber = disassembledPeselSum[2];
-        if (thirdNumber == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        int peselSum = (disassembledPesel[1] * 3) + (disassembledPesel[5] * 3) + (disassembledPesel[9] * 3) + (disassembledPesel[2] * 7) + (disassembledPesel[6] * 7) + (disassembledPesel[3] * 9) + (disassembledPesel[7] * 9) + disassembledPesel[0] + disassembledPesel[4] + disassembledPesel[8] + disassembledPesel[10];
+        return Integer.toString(peselSum).endsWith("0");
     }
 
 
