ecf3a2c6dbfb87e6ed70f746196e3a35a576d712
dnuker <rix4141@gmail.com>
2023-02-12 01:49:28 +0100

    done, waiting for review

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 
 public class PrimeNumberGenerator {
 
-    public static List<Integer> dividesByTwoThreeFiveSeven(int startNumber, int endNumber) {
+    public static List<Integer> dividesByTwoThreeFiveSeven(int endNumber) {
         List<Integer> listOfDividedNumbers = new ArrayList();
-        if(startNumber<=7){
+        int startNumber = 2;
+        if (startNumber <= 7) {
             listOfDividedNumbers.add(2);
             listOfDividedNumbers.add(3);
             listOfDividedNumbers.add(5);
             listOfDividedNumbers.add(7);
         }
         for (int i = startNumber; i <= endNumber; i++) {
-            if(i==1){
+            if (i == 1) {
                 i++;
             }
             if (i % 2 != 0) {
@@ -30,30 +31,34 @@ public class PrimeNumberGenerator {
         return listOfDividedNumbers;
     }
 
+    public static List<Integer> deletesMultipliedNumbers(List<Integer> listOfDividedNumbers, int endNumber) {
+        int numberMultiplied = 0;
+        for (int i = 2; i < endNumber; i++) {
+            for (int j = 2; j < Math.sqrt(endNumber); j++) {
+                numberMultiplied = i * j;
+                listOfDividedNumbers.remove(Integer.valueOf(numberMultiplied));
+            }
+        }
+        return listOfDividedNumbers;
+    }
 
- public static List<Integer> deletesMultipliedNumbers(List<Integer> listOfDividedNumbers, int endNumber) {
-
-     int numberMultiplied = 0;
-     for (int i = 0; i<listOfDividedNumbers.size(); i++) {
-
-         for (int j = 2; j < Math.sqrt(endNumber); j++) {
-             int numberOnId = listOfDividedNumbers.get(i);
-
-             numberMultiplied =  numberOnId * j;
-             listOfDividedNumbers.remove(Integer.valueOf(numberMultiplied));
-
-         }
-     }
-     return listOfDividedNumbers;
- }
+    public static List<Integer> takeCompartmentOfNumbers(List<Integer> listOfNumbers, int startNumber) {
+        int sizeOfList = listOfNumbers.size();
+        for (int i = 0; i < sizeOfList; i++) {
+            if (listOfNumbers.get(0) < startNumber) {
+                listOfNumbers.remove(0);
+            }
+        }
+        return listOfNumbers;
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int startNumber = scan.nextInt();
         int endNumber = scan.nextInt();
-        List<Integer> listOfDividedNumbers = dividesByTwoThreeFiveSeven(startNumber,endNumber);
-
-        System.out.print(deletesMultipliedNumbers(listOfDividedNumbers,endNumber));
+        List<Integer> listOfDividedNumbers = deletesMultipliedNumbers(dividesByTwoThreeFiveSeven(endNumber), endNumber);
+        List<Integer> listOfPrimeNumbers = takeCompartmentOfNumbers(listOfDividedNumbers, startNumber);
+        System.out.print(listOfPrimeNumbers);
     }
 
 }
