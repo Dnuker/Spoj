2f124f324005a174bc37dce74f9a0a55743ae0ab
dnuker <rix4141@gmail.com>
2023-02-12 22:05:25 +0100

    ended task

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -5,49 +5,24 @@ import java.util.Scanner;
 //https://pl.spoj.com/problems/PRIME1/
 public class PrimeNumberGenerator {
 
-  /*  public static List<Integer> dividesByTwoThreeFiveSeven(int endNumber) {
-        List<Integer> listOfDividedNumbers = new ArrayList();
-        int startNumber = 2;
-        if (startNumber <= 7) {
-            listOfDividedNumbers.add(2);
-            listOfDividedNumbers.add(3);
-            listOfDividedNumbers.add(5);
-            listOfDividedNumbers.add(7);
-        }
-        for (int i = startNumber; i <= endNumber; i++) {
-            if (i == 1) {
-                i++;
-            }
-            if (i % 2 != 0) {
-                if (i % 3 != 0) {
-                    if (i % 5 != 0) {
-                        if (i % 7 != 0) {
-                            listOfDividedNumbers.add(i);
-                        }
-                    }
-                }
-            }
-        }
-        return listOfDividedNumbers;
-    }*/
 
-    public static List<Integer> deletesMultipliedNumbers(int endNumber) {
-        List<Integer> listOfDividedNumbers = new ArrayList();
-        for(int number = 2;number<=endNumber;number++){
-            listOfDividedNumbers.add(number);
+    public static List<Integer> findPrimeNumbers(int endNumber) {
+        List<Integer> primeNumbers = new ArrayList();
+        for (int number = 2; number <= endNumber; number++) {
+            primeNumbers.add(number);
         }
-        //List<Integer> justForCheck = listOfDividedNumbers;
+
         int numberMultiplied = 0;
         for (int i = 2; i < endNumber; i++) {
             for (int j = 2; j < Math.sqrt(endNumber); j++) {
                 numberMultiplied = i * j;
-                listOfDividedNumbers.remove(Integer.valueOf(numberMultiplied));
+                primeNumbers.remove(Integer.valueOf(numberMultiplied));
             }
         }
-        return listOfDividedNumbers;
+        return primeNumbers;
     }
 
-    public static List<Integer> takeCompartmentOfNumbers(List<Integer> listOfNumbers, int startNumber) {
+    public static List<Integer> primeNumbersInRange(List<Integer> listOfNumbers, int startNumber) {
         int sizeOfList = listOfNumbers.size();
         for (int i = 0; i < sizeOfList; i++) {
             if (listOfNumbers.get(0) < startNumber) {
@@ -63,9 +38,9 @@ public class PrimeNumberGenerator {
         for (int i = 0; i < testCaseCount; i++) {
             int startNumber = scan.nextInt();
             int endNumber = scan.nextInt();
-            List<Integer> listOfDividedNumbers = deletesMultipliedNumbers(endNumber);
-            List<Integer> listOfPrimeNumbers = takeCompartmentOfNumbers(listOfDividedNumbers, startNumber);
-            System.out.print(listOfPrimeNumbers);
+            List<Integer> primeNumbers = findPrimeNumbers(endNumber);
+            List<Integer> primeNumbersInRange = primeNumbersInRange(primeNumbers, startNumber);
+            System.out.print(primeNumbersInRange);
         }
     }
 
